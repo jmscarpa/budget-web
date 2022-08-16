39d5404cd066e2d063f7496aa66846613d109845
Pedro <pedrotherealone1@gmail.com>
2022-08-16 13:28:37 -0300

    feat: init layout following andela model

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,9 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-layout',
     templateUrl: './layout.component.html',
+    styleUrls: ['./layout.component.scss']
 })
 
 export class LayoutComponent {
